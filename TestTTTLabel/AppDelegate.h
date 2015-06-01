f2041f231c2e7b948fb366db5673fd60c3a1dab7
Michel Mongkhoy <michel.mongkhoy@gmail.com>
2015-06-01 23:00:51 +0200

    Initial Commit

---
+++ TestTTTLabel/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestTTTLabel
+//
+//  Created by Michel Mongkhoy on 6/1/15.
+//  Copyright (c) 2015 Michel Mongkhoy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TestTTTLabel/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestTTTLabel
+//
+//  Created by Michel Mongkhoy on 6/1/15.
+//  Copyright (c) 2015 Michel Mongkhoy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
